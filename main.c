41844619a817e8c67bd99fb6245cb57eedc4a29f
yucha5 <yuyukhkh0533@gmail.com>
2026-07-02 17:58:19 +0900

    kadai2 added

--- main.c
+++ main.c
@@ -30,6 +30,16 @@ int main()
     }
 
   printf("Heads: %d, Tails: %d\n", head, tail);
+  
+  if(heads > tails)
+  {
+    printf("You won\n");
+  }
+
+  else
+  {
+    printf("You lost\n");
+  }
 
   return 0;
 }
